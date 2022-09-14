775de248b7236598c743fffd00c01c7e50c0587f
Sumanta Kabiraj <sumantablog@gmail.com>
2022-09-14 10:50:02 +0530

    assignment Complete

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,7 +1,76 @@
 import React, { Component } from "react";
 
 class Countdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerOn: false,
+      timerStart: 0,
+      timerTime: 0,
+    };
+  }
+
+  startTimer = () => {
+    this.setState({
+      timerOn: true,
+      timerTime: this.state.timerTime,
+      timerStart: this.state.timerTime,
+    });
+    this.timer = setInterval(() => {
+      const newTime = this.state.timerTime - 10;
+      console.log(`Time left is 🚀 ${newTime}`);
+      if (newTime >= 0) {
+        this.setState({
+          timerTime: newTime,
+        });
+      } else {
+        clearInterval(this.timer);
+        this.setState({ timerOn: false });
+        alert("Countdown ended");
+      }
+    }, 10);
+  };
+
+  stopTimer = () => {
+    clearInterval(this.timer);
+    this.setState({ timerOn: false });
+  };
+  resetTimer = () => {
+    if (this.state.timerOn === false) {
+      this.setState({
+        timerTime: this.state.timerStart,
+      });
+    }
+  };
+
+  adjustTimer = (input) => {
+    const { timerTime, timerOn } = this.state;
+    if (!timerOn) {
+      if (input === "incHours" && timerTime + 3600000 < 216000000) {
+        this.setState({ timerTime: timerTime + 3600000 });
+      } else if (input === "decHours" && timerTime - 3600000 >= 0) {
+        this.setState({ timerTime: timerTime - 3600000 });
+      } else if (input === "incMinutes" && timerTime + 60000 < 216000000) {
+        this.setState({ timerTime: timerTime + 60000 });
+      } else if (input === "decMinutes" && timerTime - 60000 >= 0) {
+        this.setState({ timerTime: timerTime - 60000 });
+      } else if (input === "incSeconds" && timerTime + 1000 < 216000000) {
+        this.setState({ timerTime: timerTime + 1000 });
+      } else if (input === "decSeconds" && timerTime - 1000 >= 0) {
+        this.setState({ timerTime: timerTime - 1000 });
+      }
+    }
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
   render() {
+    const { timerTime, timerStart, timerOn } = this.state;
+    let seconds = ("0" + (Math.floor((timerTime / 1000) % 60) % 60)).slice(-2);
+    let minutes = ("0" + Math.floor((timerTime / 60000) % 60)).slice(-2);
+    let hours = ("0" + Math.floor((timerTime / 3600000) % 60)).slice(-2);
+
     return (
       <>
         <div
@@ -16,19 +85,78 @@ class Countdown extends Component {
             <div className="card-subtitle">Hours : Minutes : Seconds</div>
             <div className="card-body">
               <div className="card-flex">
-                <button className="countdown-btn">⬆</button>
-                <button className="countdown-btn">⬆</button>
-                <button className="countdown-btn">⬆</button>
+                <button
+                  className="countdown-btn"
+                  onClick={() => this.adjustTimer("incHours")}
+                >
+                  ⬆
+                </button>
+                <button
+                  className="countdown-btn"
+                  onClick={() => this.adjustTimer("incMinutes")}
+                >
+                  ⬆
+                </button>
+                <button
+                  className="countdown-btn"
+                  onClick={() => this.adjustTimer("incSeconds")}
+                >
+                  ⬆
+                </button>
               </div>
-              <div className="countdown-timer">00 : 00 : 00</div>
+              <div className="countdown-timer">
+                {hours} : {minutes} : {seconds}
+              </div>
               <div className="card-flex">
-                <button className="countdown-btn">⬇</button>
-                <button className="countdown-btn">⬇</button>
-                <button className="countdown-btn">⬇</button>
+                <button
+                  className="countdown-btn"
+                  onClick={() => this.adjustTimer("decHours")}
+                >
+                  ⬇
+                </button>
+                <button
+                  className="countdown-btn"
+                  onClick={() => this.adjustTimer("decMinutes")}
+                >
+                  ⬇
+                </button>
+                <button
+                  className="countdown-btn"
+                  onClick={() => this.adjustTimer("decSeconds")}
+                >
+                  ⬇
+                </button>
               </div>
             </div>
             <div className="card-footer">
-              <button>Start</button>
+              {/* <button>Start</button> */}
+              {timerOn === false &&
+                (timerStart === 0 || timerTime === timerStart) && (
+                  <button className="Button-start" onClick={this.startTimer}>
+                    Start
+                  </button>
+                )}
+              {timerOn === true && timerTime >= 1000 && (
+                <button className="Button-stop" onClick={this.stopTimer}>
+                  Stop
+                </button>
+              )}
+              {timerOn === false &&
+                timerStart !== 0 &&
+                timerStart !== timerTime &&
+                timerTime !== 0 && (
+                  <button className="Button-start" onClick={this.startTimer}>
+                    Resume
+                  </button>
+                )}
+
+              {(timerOn === false || timerTime < 1000) &&
+                timerStart !== timerTime &&
+                timerStart > 0 && (
+                  <button className="Button-reset" onClick={this.resetTimer}>
+                    Reset
+                  </button>
+                )}
             </div>
           </div>
         </div>
--- src/StopWatch.js
+++ src/StopWatch.js
@@ -1,7 +1,51 @@
 import React, { Component } from "react";
 
 class StopWatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerOn: false,
+      timerStart: 0,
+      timerTime: 0,
+    };
+  }
+
+  startTimer = () => {
+    this.setState({
+      timerOn: true,
+      timerTime: this.state.timerTime,
+      timerStart: Date.now() - this.state.timerTime,
+    });
+
+    this.timer = setInterval(() => {
+      this.setState({
+        timerTime: Date.now() - this.state.timerStart,
+      });
+    }, 10);
+  };
+
+  stopTimer = () => {
+    this.setState({ timerOn: false });
+    clearInterval(this.timer);
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  resetTimer = () => {
+    this.setState({
+      timerStart: 0,
+      timerTime: 0,
+    });
+  };
+
   render() {
+    const { timerTime } = this.state;
+    let miliSeconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+    let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+    let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
     return (
       <>
         <div
@@ -13,9 +57,22 @@ class StopWatch extends Component {
         <div className="card">
           <div className="flex-card">
             <div className="card-title">Stopwatch</div>
-            <div className="card-body stopwatch-timer">00 : 00 : 00 : 00</div>
+            <div className="card-body stopwatch-timer">
+              {hours} : {minutes} : {seconds} : {miliSeconds}
+            </div>
             <div className="card-footer">
-              <button>Start</button>
+              {this.state.timerOn === false && this.state.timerTime === 0 && (
+                <button onClick={this.startTimer}>Start</button>
+              )}
+              {this.state.timerOn === true && (
+                <button onClick={this.stopTimer}>Stop</button>
+              )}
+              {this.state.timerOn === false && this.state.timerTime > 0 && (
+                <button onClick={this.startTimer}>Resume</button>
+              )}
+              {this.state.timerOn === false && this.state.timerTime > 0 && (
+                <button onClick={this.resetTimer}>Reset</button>
+              )}
             </div>
           </div>
         </div>
